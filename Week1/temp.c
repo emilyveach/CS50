b7121961b7c72612266a21c5a7bffe8d1f12d74c
Emily Veach <veachemily@gmail.com>
2014-01-15 04:51:03 -0500

    temperature converter

---
+++ Week1/temp.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//converting F to C
+int main(void)
+{
+    int far;
+    printf("Enter a temp in F, as an integer: ");
+    far = GetInt();
+    printf("%d in Celius is %.2f\n", far, ((far - 32.0) * (5.0 / 9.0))); 
+}
