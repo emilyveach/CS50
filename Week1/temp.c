430b73898eaecc4956b01ee31f207fd28d43f795
Emily Veach <veachemily@gmail.com>
2014-01-15 05:45:40 -0500

    Added some color

--- Week1/temp.c
+++ Week1/temp.c
@@ -5,7 +5,22 @@
 int main(void)
 {
     int far;
+    float cel;
     printf("Enter a temp in F, as an integer: ");
     far = GetInt();
-    printf("%d in Celsius is %.2f\n", far, ((far - 32.0) * (5.0 / 9.0))); 
+    cel = (far - 32.0) * (5.0 / 9.0);
+    printf("%d in Celsius is %.2f. ", far, cel);
+    
+    if (cel <= 0)
+    {
+        printf("Brrr.\n");
+    }
+    else if (cel >= 100)
+    {
+        printf("Hey, guy, you're boilin'.\n");
+    }
+    else
+    {
+        printf("\n");
+    }
 }
