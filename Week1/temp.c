5b5aa82b9bf6b113479fb8c36be32354e2d3becf
Emily Veach <veachemily@gmail.com>
2014-01-15 05:06:10 -0500

    fixed spelling

--- Week1/temp.c
+++ Week1/temp.c
@@ -7,5 +7,5 @@ int main(void)
     int far;
     printf("Enter a temp in F, as an integer: ");
     far = GetInt();
-    printf("%d in Celius is %.2f\n", far, ((far - 32.0) * (5.0 / 9.0))); 
+    printf("%d in Celsius is %.2f\n", far, ((far - 32.0) * (5.0 / 9.0))); 
 }
